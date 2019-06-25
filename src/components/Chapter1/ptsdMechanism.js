8549d5ebd56c7e363661246c29e2de6f8c5b261b
zawadka <louis.78100@hotmail.fr>
2019-06-26 00:26:27 +0200

    video

---
+++ src/components/Chapter1/ptsdMechanism.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ptsdMechanism extends Component {
+  render () {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default ptsdMechanism
