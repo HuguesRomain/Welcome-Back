782edb426c30fe322eda4b4436cf081f9749f4f2
MaksymY <maksym.yankivskyypro@gmail.com>
2019-06-26 14:04:31 +0200

    add animation of home

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -4,19 +4,30 @@ import ContentLandingPage from './ContentLandingPage'
 import Button from './Button'
 import Video from './Video'
 import Audio from './AudioBackground'
+import {TimelineLite} from 'gsap'
 
 class LandingPage extends Component {
+  constructor(props){
+    super(props);
+    this.tween = new TimelineLite({ paused:false });
+
+    this.back = null;
+  }
+
+  componentDidMount(){
+    this.tween
+      .to(this.back, 2,{ opacity: 0 }, 2)
+  }
   render () {
     return (
       <Fragment>
-       
         <Audio />
         <div className='landingPage'>
         
           <div className='content__LandingPage'> 
           
           <Video  />
-          <div className='divBlack'></div>
+          <div className='divBlack' ref={ div => this.back = div }></div>
             <TitleLandingPage />
             <ContentLandingPage />
             <Button name={'Start the story'} />    
